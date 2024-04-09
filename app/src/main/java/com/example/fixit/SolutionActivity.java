6a3cb6f5338dda26f5fa356d8f572cedce4b17b9
HOCINE Mohand Ouidir <hocinidir26@gmail.com>
2024-04-09 16:44:41 +0100

    Initialize the solution activity

---
+++ app/src/main/java/com/example/fixit/SolutionActivity.java
@@ -0,0 +1,14 @@
+package com.example.fixit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SolutionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solution);
+    }
+}
